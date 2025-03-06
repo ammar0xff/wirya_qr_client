51a00336f83c950141cf242f342b6f2cfa40c22f
Ammar Mohamed <ammar0xf@gmail.com>
2025-03-06 15:41:49 +0200

    code magic

--- lib/utils/permissions_handler.dart
+++ lib/utils/permissions_handler.dart
@@ -1,23 +1,13 @@
-import 'package:geolocator/geolocator.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class PermissionsHandler {
   static Future<void> requestPermissions() async {
-    try {
-      LocationPermission permission = await Geolocator.checkPermission();
-      if (permission == LocationPermission.denied) {
-        permission = await Geolocator.requestPermission();
-      }
-      if (permission == LocationPermission.deniedForever) {
-        // Permissions are denied forever, handle appropriately.
-        throw Exception("Location permissions are permanently denied.");
-      }
-      if (permission == LocationPermission.denied) {
-        // Permissions are denied, handle appropriately.
-        throw Exception("Location permissions are denied.");
-      }
-    } catch (e) {
-      print("Error requesting permissions: $e");
-      throw e;
-    }
+    await [
+      Permission.location,
+      Permission.locationAlways,
+      Permission.locationWhenInUse,
+      Permission.notification,
+      Permission.ignoreBatteryOptimizations,
+    ].request();
   }
 }
