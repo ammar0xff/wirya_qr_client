57918768d14d3ee303bf460a3feb9dceb8e324b1
Ammar Mohamed <ammar0xf@gmail.com>
2025-03-06 10:14:03 +0200

    code magic

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,7 @@ import 'screens/login_screen.dart'; // Import Login Screen
 import 'utils/permissions_handler.dart'; // Import Permissions Handler
 import 'utils/location_service.dart'; // Import Location Service
 import 'utils/battery_optimization_handler.dart'; // Import Battery Optimization Handler
-import 'package:geolocator/geolocator.dart'; // Import Geolocator package
-import 'package:workmanager/workmanager.dart'; // Import WorkManager
+import 'package:flutter_background_service/flutter_background_service.dart'; // Import Flutter Background Service
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -26,36 +25,46 @@ void main() async {
       LocationService.startLocationUpdates(username);
     }
 
-    await initializeWorkManager();
+    await initializeService();
   } catch (e) {
     runApp(ErrorApp("Failed to initialize app: $e"));
   }
 }
 
-void callbackDispatcher() {
-  Workmanager().executeTask((task, inputData) async {
-    // Perform background task
-    SharedPreferences prefs = await SharedPreferences.getInstance();
+Future<void> initializeService() async {
+  final service = FlutterBackgroundService();
+
+  await service.configure(
+    androidConfiguration: AndroidConfiguration(
+      onStart: onStart,
+      autoStart: true,
+      isForegroundMode: false, // Set to false to avoid showing notifications
+    ),
+    iosConfiguration: IosConfiguration(
+      onForeground: onStart,
+      onBackground: onIosBackground,
+    ),
+  );
+
+  service.startService();
+}
+
+void onStart(ServiceInstance service) {
+  service.on('stopService').listen((event) {
+    service.stopSelf();
+  });
+
+  SharedPreferences.getInstance().then((prefs) {
     String? username = prefs.getString('username');
     if (username != null) {
-      Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-      LocationService.updateLocation(username, position);
+      LocationService.startLocationUpdates(username);
     }
-    return Future.value(true);
   });
 }
 
-Future<void> initializeWorkManager() async {
-  await Workmanager().initialize(
-    callbackDispatcher,
-    isInDebugMode: true, // Set to false in production
-  );
-
-  await Workmanager().registerPeriodicTask(
-    "1",
-    "simplePeriodicTask",
-    frequency: Duration(minutes: 15), // Run every 15 minutes
-  );
+bool onIosBackground(ServiceInstance service) {
+  WidgetsFlutterBinding.ensureInitialized();
+  return true;
 }
 
 class QRClientApp extends StatelessWidget {
