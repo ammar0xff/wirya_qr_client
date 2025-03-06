5244268f681c3c08e23c3e0fa5d9dea9568f2d4b
Ammar Mohamed <ammar0xf@gmail.com>
2025-03-06 10:53:03 +0200

    code magic

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'utils/permissions_handler.dart'; // Import Permissions Handler
 import 'utils/location_service.dart'; // Import Location Service
 import 'utils/battery_optimization_handler.dart'; // Import Battery Optimization Handler
 import 'package:flutter_background_service/flutter_background_service.dart'; // Import Flutter Background Service
+import 'package:flutter_local_notifications/flutter_local_notifications.dart'; // Import Flutter Local Notifications
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -38,7 +39,11 @@ Future<void> initializeService() async {
     androidConfiguration: AndroidConfiguration(
       onStart: onStart,
       autoStart: true,
-      isForegroundMode: false, // Set to false to avoid showing notifications
+      isForegroundMode: true, // Set to true to ensure the service runs continuously
+      notificationChannelId: 'my_foreground',
+      initialNotificationTitle: 'QR Client',
+      initialNotificationContent: 'App is running in the background',
+      foregroundServiceNotificationId: 888,
     ),
     iosConfiguration: IosConfiguration(
       onForeground: onStart,
@@ -49,7 +54,35 @@ Future<void> initializeService() async {
   service.startService();
 }
 
-void onStart(ServiceInstance service) {
+void onStart(ServiceInstance service) async {
+  if (service is AndroidServiceInstance) {
+    final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+    const AndroidNotificationChannel channel = AndroidNotificationChannel(
+      'my_foreground', // id
+      'Foreground Service', // title
+      description: 'This channel is used for the foreground service.', // description
+      importance: Importance.low, // Set importance to low
+    );
+
+    await flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
+        ?.createNotificationChannel(channel);
+
+    service.setForegroundNotificationInfo(
+      title: "QR Client",
+      content: "App is running in the background",
+    );
+
+    service.on('setAsForeground').listen((event) {
+      service.setAsForegroundService();
+    });
+
+    service.on('setAsBackground').listen((event) {
+      service.setAsBackgroundService();
+    });
+  }
+
   service.on('stopService').listen((event) {
     service.stopSelf();
   });
